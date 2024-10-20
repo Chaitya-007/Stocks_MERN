8705ed813d9470f23b13a999ee795f818b951330
Chaitanya <chaitanyaasole@gmail.com>
2024-10-20 13:09:16 +0530

    never use ; after PORT in .env

--- backend/index.js
+++ backend/index.js
@@ -6,6 +6,10 @@ const PORT = process.env.PORT || 5000;
 
 const app = express();
 
+app.get("/", (request, response) => {
+  response.send("Server is ready");
+});
+
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
