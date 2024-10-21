51b2d3578edbd1944feb4e32c3f238f2dbd4978c
Chaitanya <chaitanyaasole@gmail.com>
2024-10-21 23:39:17 +0530

    Backbutton in components

---
+++ frontend/src/components/BackButton.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const BackButton = () => {
+  return <div className="bg-green-500 text-3xl text-white">BackButton</div>;
+};
+
+export default BackButton;
--- frontend/src/pages/EditStock.jsx
+++ frontend/src/pages/EditStock.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import 
 
 const EditStock = () => {
   return <div>EditStock</div>;
