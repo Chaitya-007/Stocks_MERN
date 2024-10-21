8fd2edd9c24f73c8b517b9a232163feefbcf06fa
Chaitanya <chaitanyaasole@gmail.com>
2024-10-21 23:32:53 +0530

    Created List OF Stocks

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,8 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
-import CreateBook from "./pages/CreateBook";
-import DeleteBook from "./pages/DeleteBook";
-import EditBook from "./pages/EditBook";
+import CreateBook from "./pages/CreateStock";
+import DeleteBook from "./pages/DeleteStock";
+import EditBook from "./pages/EditStock";
 import Home from "./pages/Home";
 
 const App = () => {
---
+++ frontend/src/pages/CreateStock.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const CreateStock = () => {
+  return <div>CreateStock</div>;
+};
+
+export default CreateStock;
