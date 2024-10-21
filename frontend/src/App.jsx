66e2b1135dc09e39be564eddeb7e40df9f31c66b
Chaitanya <chaitanyaasole@gmail.com>
2024-10-21 20:33:24 +0530

    added path and element in app.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,19 @@
 import React from "react";
-import {Routes, Route} from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
+import CreateBook from "./pages/CreateBook";
+import DeleteBook from "./pages/DeleteBook";
+import EditBook from "./pages/EditBook";
+import Home from "./pages/Home";
 
 const App = () => {
-  return <Routes>
-    <Route path="" element={}/>
-    <Route path="" element={}/>
-    <Route path="" element={}/>
-    <Route path="" element={}/>
-  </Routes>
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/books/create" element={<CreateBook />} />
+      <Route path="/books/edit/:id" element={<EditBook />} />
+      <Route path="/delete/:id" element={<DeleteBook />} />
+    </Routes>
+  );
 };
 
 export default App;
