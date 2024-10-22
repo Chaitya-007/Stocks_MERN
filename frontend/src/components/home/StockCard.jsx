23cdffefb1d1ed8712e190e24556ea0376bcb6db
Chaitanya <chaitanyaasole@gmail.com>
2024-10-22 09:05:02 +0530

    Passed data to StockSingleCard.jsx

--- frontend/src/components/home/StockCard.jsx
+++ frontend/src/components/home/StockCard.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import StockSingleCard from "./StockSingleCard";
 
 const StockCard = ({ stocks }) => {
   return (
@@ -7,28 +8,7 @@ const StockCard = ({ stocks }) => {
       StockCard
       <div>
         {stocks.map((stock, index) => (
-          <div key={index} className="p-4 my-4 border-4 border-black">
-            <h2 className="text-xl">{stock.name}</h2>
-            <p className="text-lg">{stock.price}</p>
-            <p className="text-lg">{stock.marketCap}</p>
-            <div className="flex">
-              <Link to={`/stocks/edit/${stock._id}`}>
-                <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
-                  Edit
-                </button>
-              </Link>
-              <Link to={`/stocks/details/${stock._id}`}>
-                <button className="bg-yellow-400 hover:bg-yellow-700 text-black font-bold py-2 px-4 rounded">
-                  Show
-                </button>
-              </Link>
-              <Link to={`/stocks/delete/${stock._id}`}>
-                <button className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded">
-                  Delete
-                </button>
-              </Link>
-            </div>
-          </div>
+          <StockSingleCard key={index} stock={stock}></StockSingleCard>
         ))}
       </div>
     </div>
--- frontend/src/components/home/StockSingleCard.jsx
+++ frontend/src/components/home/StockSingleCard.jsx
@@ -1,7 +1,31 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
-const StockSingleCard = () => {
-  return <div>StockSingleCard</div>;
+const StockSingleCard = ({ stock }) => {
+  return (
+    <div className="p-4 my-4 border-4 border-black">
+      <h2 className="text-xl">{stock.name}</h2>
+      <p className="text-lg">{stock.price}</p>
+      <p className="text-lg">{stock.marketCap}</p>
+      <div className="flex">
+        <Link to={`/stocks/edit/${stock._id}`}>
+          <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+            Edit
+          </button>
+        </Link>
+        <Link to={`/stocks/details/${stock._id}`}>
+          <button className="bg-yellow-400 hover:bg-yellow-700 text-black font-bold py-2 px-4 rounded">
+            Show
+          </button>
+        </Link>
+        <Link to={`/stocks/delete/${stock._id}`}>
+          <button className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded">
+            Delete
+          </button>
+        </Link>
+      </div>
+    </div>
+  );
 };
 
 export default StockSingleCard;
